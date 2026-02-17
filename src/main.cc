c141613fbcc447b71f77737da00ca281455f60ef
Alexey <lavrik-aleksey-1@yandex.ru>
2026-02-17 13:12:01 +0300

    added opencl config file

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,3 @@
-#define CL_HPP_ENABLE_EXCEPTIONS
-#define CL_HPP_TARGET_OPENCL_VERSION 120
-#define CL_HPP_MINIMUM_OPENCL_VERSION 120
-
-#include <CL/opencl.hpp>
-
 #include <iostream>
 #include <memory>
 #include <vector>
